f7cc3d8d9a39ae4e3d40fe08d083ff3cab9b1976
Myles McNamara <myles@smyl.es>
2023-01-27 17:20:27 -0500

    create log dir if not exists

--- h-run.sh
+++ h-run.sh
@@ -3,4 +3,7 @@
 killall -9 rigel
 . h-manifest.conf
 
+CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
+[[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
+
 ./rigel $(< $CUSTOM_CONFIG_FILENAME) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
