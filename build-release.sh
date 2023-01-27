66330cd253c683f6c9c0a21504c9a7d1269efc56
Myles McNamara <myles@smyl.es>
2023-01-27 18:42:07 -0500

    add _hXX for versioning of this file, adjust build release script

--- build-release.sh
+++ build-release.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-VERSION=1.3.2
+RIGEL_VERSION=1.3.2
+THIS_VERSION=h2
 
+RELEASE_VERSION="${RIGEL_VERSION}_${THIS_VERSION}"
+
+rm -rf dist/rigelvipor-$RELEASE_VERSION.tar.gz
 mkdir temp
 cd temp
-wget https://github.com/rigelminer/rigel/releases/download/$VERSION/rigel-$VERSION-linux.tar.gz
-tar -xvf rigel-$VERSION-linux.tar.gz
-cp rigel-$VERSION-linux/rigel ../rigelvipor/rigel
+wget https://github.com/rigelminer/rigel/releases/download/$RIGEL_VERSION/rigel-$RIGEL_VERSION-linux.tar.gz
+tar -xvf rigel-$RIGEL_VERSION-linux.tar.gz
+cp rigel-$RIGEL_VERSION-linux/rigel ../rigelvipor/rigel
 cd ..
 rm -rf temp
-tar -zcvf dist/rigelvipor-$VERSION.tar.gz rigelvipor
+
+
+tar -zcvf dist/rigelvipor-$RELEASE_VERSION.tar.gz rigelvipor
 rm -rf rigelvipor/rigel
