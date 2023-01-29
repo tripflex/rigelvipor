0911c7c1fe84a21f09a1c41fcbb2cedc0a959c9f
Myles McNamara <myles@smyl.es>
2023-01-29 10:46:49 -0500

    add --no-tui to get output in hive "miner" and "motd watch" screen

--- rigelvipor/h-config.sh
+++ rigelvipor/h-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This code is included in /hive/bin/custom function
-conf=" --algorithm $CUSTOM_ALGO --url stratum+tcp://pool.vipor.net:$CUSTOM_URL --username $CUSTOM_TEMPLATE --api-bind 0.0.0.0:5000 --log-file $CUSTOM_LOG_BASENAME.log"
+conf=" --algorithm $CUSTOM_ALGO --url stratum+tcp://pool.vipor.net:$CUSTOM_URL --username $CUSTOM_TEMPLATE --api-bind 0.0.0.0:5000 --no-tui --log-file $CUSTOM_LOG_BASENAME.log"
 [[ ! -z $CUSTOM_USER_CONFIG ]] && conf+=" $CUSTOM_USER_CONFIG"
 
 echo "$conf"
